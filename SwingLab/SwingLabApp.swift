67931a553aa1d8fad8fd8394c3296e75a3f552c4
Mihnea Popescu <mihnea.popescu56@gmail.com>
2026-09-04 18:18:02 -0400

    Initial Commit

---
+++ SwingLab/SwingLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingLabApp.swift
+//  SwingLab
+//
+//  Created by Mihnea Popescu on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SwingLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
